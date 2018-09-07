8bd685d5ff9934e1065c0a080596afbb47436f5c
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-09-07 10:12:42 +0200

    Clean iterative CNN

--- iterative-cnn/iterative_cnn/data.py
+++ iterative-cnn/iterative_cnn/data.py
@@ -1,5 +1,3 @@
-import os
-import numpy as np
 import pandas as pd
 
 
@@ -9,31 +7,3 @@ def get_ondrejov_dataset(dataset_file):
     ondrejov_flux = ondrejov_dataset.iloc[:, 12:].values
     ondrejov_labels = ondrejov_dataset['label'].values
     return ondrejov_flux, ondrejov_labels
-
-
-def extract_filename(path):
-    """Remove directories from path and return only filename."""
-    return os.path.basename(path)
-
-
-vec_extract_filename = np.vectorize(extract_filename)
-
-
-def query_spectra_by_filename(filenames, hdf5):
-    """Return identifiers and fluxes of spectra with filenames in query."""
-    ids = vec_extract_filename(hdf5['id_lam'])
-    mask = np.isin(ids, filenames)
-    flux = hdf5['X_lam']
-    return ids[mask], flux[mask, :]
-
-
-def sample_non_candidates(candidates_filenames, hdf5, size, random_state=0):
-    # remove path from filenames
-    ids = vec_extract_filename(hdf5['id_lam'])
-    # get non-candidates spectra
-    mask = ~np.isin(ids, candidates_filenames)
-    non_ids, non_flux = hdf5['id_lam'][mask], hdf5['X_lam'][mask, :]
-    # get random sample
-    np.random.seed(random_state)
-    index = np.random.choice(non_ids.shape[0], size=size, replace=False)
-    return non_ids[index], non_flux[index, :]
